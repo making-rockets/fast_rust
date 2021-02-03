9e80d9d28724f803daf3950338f19476954bd108
baiyu <by965738071@163>
2021-02-03 13:52:27 +0800

    the lonely shepherd

--- fast_api/src/main.rs
+++ fast_api/src/main.rs
@@ -1,30 +1,34 @@
-use lazy_static::lazy_static;
-use tokio::runtime::Builder;
-use tokio::runtime::Runtime;
+use futures::future::join_all;
+use reqwest::{Response, Error};
+use std::future::Future;
 
-use async_once::AsyncOnce;
-
-lazy_static! {
-    static ref FOO : AsyncOnce<u32> = AsyncOnce::new(async{
-        1
-    });
-}
-fn main() {
-
-    let mut rt =  Builder::new().thread_name("111").build().unwrap();   //Builder::new_current_thread().build().unwrap();
-    rt.block_on(async {
-        assert_eq!(FOO.get().await , &1)
-    });
-
-    #[derive(Debug)]
-    struct  abc{
-        a:u32,
-        b:u32,
+async fn fetch_path(path: String) -> Result<(), reqwest::Error> {
+    match reqwest::get(&path).await {
+        Ok(resp) => {
+            match resp.text().await {
+                Ok(text) =>{
+                    println!("{:?}", text);
+                    println!("{:?}",text.len())
+                }
+                Err(_) =>{
+                    println!("error while reading {}",path)
+                }
+            }
+        }
+        Err(_) => {
+            println!("error while scraping from {}",path)
+        }
     }
-
-    let mut x = abc { a: 12, b: 13 };
-    //let mut  y = &mut  x;
-    x.a = 14;
-   // y.a = 15;
-    //println!("{:?}",y);
+    Ok(())
 }
+
+#[tokio::main]
+async fn main() -> Result<(), reqwest::Error> {
+    let paths = vec!["http://xxx.xxx.com/xxx".to_string(), "http://xxx.xxx.com/xxx".to_string()];
+
+    join_all(paths.into_iter().map(|path| {
+        fetch_path(path)
+    })).await;
+    Ok(())
+}
+
