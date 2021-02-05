581fe353dfe14c59f92b4e3ec81e36768f04b122
by965738071@163.com <by965738071@163.com>
2021-02-06 00:45:45 +0800

    add router

---
+++ fast_common/src/models/menu.rs
@@ -0,0 +1,32 @@
+use crate::rbatis;
+use chrono::NaiveDateTime;
+use serde_derive::{Deserialize, Serialize};
+
+#[crud_enable]
+#[derive(Debug, Serialize, Deserialize, Clone, PartialEq)]
+pub struct Menu {
+    pub id: Option<u64>,
+    pub name: Option<String>,
+    pub parent_id: Option<u64>,
+    pub path: Option<String>,
+    pub icon: Option<String>,
+    pub index_no: Option<i32>,
+    pub remark: Option<String>,
+    pub state: Option<i32>,
+    pub create_time: Option<NaiveDateTime>,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone, PartialEq)]
+pub struct MenuVo {
+    pub id: Option<u64>,
+    pub name: Option<String>,
+    pub parent_id: Option<u64>,
+    pub path: Option<String>,
+    pub icon: Option<String>,
+    pub index_no: Option<i32>,
+    pub remark: Option<String>,
+    pub state: Option<i32>,
+    pub create_time: Option<NaiveDateTime>,
+    pub page_num: Option<u64>,
+    pub page_size: Option<u64>,
+}
--- fast_demo/src/main.rs
+++ fast_demo/src/main.rs
@@ -1,12 +1,12 @@
 use chrono::format::StrftimeItems;
 use chrono::{NaiveDate, NaiveDateTime, Local};
-
+#[derive(Debug)]
 struct BaseModel {
     id: u64,
     name: String,
 }
 
-
+#[derive(Debug)]
 struct Person {
     age: u32,
     school: String,
@@ -15,10 +15,18 @@ struct Person {
 
 impl Person {
 
-    fn new(){
-
+    fn new(age:u32,school:String,baseModel:BaseModel) ->Self{
+        Self{
+            age,
+            school,
+            baseModel
+        }
     }
+
 }
 
 
-fn main() {}
+fn main() {
+    let person = Person::new(1, "1".to_string(), BaseModel { id: 0, name: "".to_string() });
+    println!("{:?}", person);
+}
