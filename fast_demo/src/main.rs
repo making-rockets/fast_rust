599289c7f5088c0c9f9d28a89dc54a9c0c4ab1a1
baiyu <by965738071@163>
2021-03-11 14:39:02 +0800

    this is sha wanyier

--- fast_demo/src/main.rs
+++ fast_demo/src/main.rs
@@ -1,5 +1,7 @@
+use git2::Repository;
 
 fn main() {
-
-
+    let repo_root = std::env::args().nth(1).unwrap_or("../".to_string());
+    let repo = Repository::open(repo_root.as_str()).expect("cloud not repository");
+    println!("{},{:?}", repo.path().display(), repo.state());
 }
