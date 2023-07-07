9c5a96a5a7a42ff1fe5d833e1604cb7e8cb7edb9
salalaika <by965738071@163.com>
2023-07-07 17:19:01 +0800

    20220707-v2

--- arysn-test/tests/common/mod.rs
+++
@@ -1,4 +0,0 @@
-#[allow(dead_code)]
-pub fn init() {
-    let _ = env_logger::builder().is_test(true).try_init();
-}
--- arysn1/docker/dev/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-TRACE_SQL=1
-export TRACE_SQL
-cd /app
-/wait-for-it.sh db:5432 -- \
-  cargo watch --ignore tmp --ignore src/generated -x 'test --features "gis" -- --nocapture'
